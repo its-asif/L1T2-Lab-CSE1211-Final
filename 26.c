3b29ac67d2422454c589848bd39a90e55ecc90a5
Asif Hossain <96633471+its-asif@users.noreply.github.com>
2022-12-09 12:10:39 +0600

    Update 26.c

--- 26.c
+++ 26.c
@@ -6,15 +6,15 @@ int main()
     FILE *fp;
     char str[100];
     char ch;
-    int i=0,lower,upper,digit,line,others,space;
-    lower=upper=digit=line=others=space=0;
+    int i=0,lower,upper,digit,line=1,others,space;
+    lower=upper=digit=others=space=0;
     fp = fopen("text.txt", "r");
     if (fp == NULL){
         printf("File doesn't exist\n");
     }
     else{
-       while(!feof(fp)){
             ch=fgetc(fp);
+       while(!feof(fp)){
             if(ch<='z' && ch>='a') lower++;
             else if(ch<='Z' && ch>='A') upper++;
             else if(ch>='0' && ch<='9') digit++;
@@ -22,8 +22,9 @@ int main()
             else if(ch==' ') space++;
             else others++;
 
+            ch=fgetc(fp);
         }
-        printf("Lowercase: %d \n uppercase: %d\n digits: %d\n line: %d\n space: %d\n special character: %d\n",lower,upper,digit, line, space, others);
+        printf(" Lowercase: %d \n uppercase: %d\n digits: %d\n line: %d\n space: %d\n special character: %d\n",lower,upper,digit, line, space, others);
 
     }
 
